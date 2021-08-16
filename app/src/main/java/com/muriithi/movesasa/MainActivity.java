98dae815270e883f6458909b775acf8f1fef4b36
Mark-Kinyua <markinyua@gmail.com>
2021-08-16 08:28:27 +0300

    Admin Module Creation

--- app/src/main/java/com/muriithi/movesasa/MainActivity.java
+++ app/src/main/java/com/muriithi/movesasa/MainActivity.java
@@ -10,8 +10,10 @@ import android.text.TextUtils;
 import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,13 +28,14 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextInputLayout reg_name, reg_date, reg_email,reg_phone,reg_username,reg_pass;
+    TextInputLayout reg_name, reg_date, reg_email,reg_phone,reg_usertype,reg_pass;
     Button reg_login,reg_sign_up;
     ImageView logo_image;
     TextView logo_text, slogan_text;
 
     FirebaseDatabase rootNode;
     DatabaseReference reference;
+    Spinner spinner = (Spinner) findViewById(R.id.usertype);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,11 +50,18 @@ public class MainActivity extends AppCompatActivity {
         reg_date = findViewById(R.id.birth_date);
         reg_email = findViewById(R.id.email);
         reg_phone = findViewById(R.id.phone_number);
-        reg_username = findViewById(R.id.username);
         reg_pass = findViewById(R.id.password);
 
         reg_sign_up = findViewById(R.id.sign_btn);
         reg_login = findViewById(R.id.login_btn);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.usertype_arrays, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
 
     }
 
@@ -61,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         Pair[] pairs = new Pair[6];
         pairs[0] = new Pair<View, String>(logo_text,"logo_text");
         pairs[1] = new Pair<View, String>(slogan_text,"sub_trans");
-        pairs[2] = new Pair<View, String>(reg_username,"user_trans");
+        pairs[2] = new Pair<View, String>(reg_usertype,"user_trans");
         pairs[3] = new Pair<View, String>(reg_pass,"pass_trans");
         pairs[4] = new Pair<View, String>(reg_login,"log_trans");
         pairs[5] = new Pair<View, String>(reg_sign_up,"sign_trans");
@@ -84,25 +94,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private Boolean validateUsername(){
-        String val = reg_username.getEditText().getText().toString();
-        String noWhitespace = "\\A\\w{4,20}\\z";
+    private Boolean validateUserType(){
 
-        if(val.isEmpty()){
-            reg_username.setError("Field Cannot be Empty");
-            return false;
-        }
-        else if(val.length()>=20){
-            reg_username.setError("Username too long");
-            return false;
-        }else if(!val.matches(noWhitespace)){
-            reg_username.setError("White spaces are not allowed");
-            return false;
-        }else{
-            reg_username.setError(null);
-            reg_username.setErrorEnabled(false);
-            return true;
-        }
+
     }
 
     private Boolean validateDate(){
@@ -174,7 +168,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void registerUser(View view) {
-        if(!validateName() | !validateDate() | !validatePassword() | !validatePhone() | !validateEmail() | !validateUsername()){
+        if(!validateName() | !validateDate() | !validatePassword() | !validatePhone() | !validateEmail() | !validateUserType()){
             return;
         }
 
@@ -185,11 +179,11 @@ public class MainActivity extends AppCompatActivity {
         String birth_date = reg_date.getEditText().getText().toString().trim();
         String email_address = reg_email.getEditText().getText().toString().trim();
         String phone_number = reg_phone.getEditText().getText().toString().trim();
-        String user_name = reg_username.getEditText().getText().toString().trim();
+        String user_type = reg_usertype.getEditText().getText().toString().trim();
         String pass_word = reg_pass.getEditText().getText().toString().trim();
 
-        RegistrationHelper registrationHelper = new RegistrationHelper(full_name, birth_date, email_address, phone_number,user_name, pass_word);
-        reference.child(user_name).setValue(registrationHelper);
+        RegistrationHelper registrationHelper = new RegistrationHelper(full_name, birth_date, email_address, phone_number,user_type, pass_word);
+        reference.child(user_type).setValue(registrationHelper);
 
         Toast.makeText(MainActivity.this, "Registration Successful",Toast.LENGTH_SHORT).show();
     }
